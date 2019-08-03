d8242f013b51deb31af713bebcd0621c383dd788
jetemi <jetemi95@hotmail.com>
2019-08-03 07:24:18 +0100

    Update admin.py

--- myscrumy/jetemi95scrumy/admin.py
+++ myscrumy/jetemi95scrumy/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import ScrumyGoals, ScrumyHistory, GoalStatus
+
+admin.site.register(ScrumyGoals)
+admin.site.register(ScrumyHistory)
+admin.site.register(GoalStatus)
 
 # Register your models here.
